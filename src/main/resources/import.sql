9b208f2c0727d21fc3dff5308dd6248da6561501
LHSDias <luizhenriquemc15@gmail.com>
2025-07-20 13:29:14 -0300

    import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -10,6 +10,7 @@ INSERT INTO tb_categoria(descricao, tipo) VALUES ('Compras', 'Variável');
 INSERT INTO tb_categoria(descricao, tipo) VALUES ('Lazer', 'Variável');
 INSERT INTO tb_categoria(descricao, tipo) VALUES ('Salário', 'Fixo');
 INSERT INTO tb_categoria(descricao, tipo) VALUES ('Vale Alimentação', 'Fixo');
+INSERT INTO tb_categoria(descricao, tipo) VALUES ('Freelancer', 'Variável');
 
 INSERT INTO tb_despesa(descricao, data_vencimento, data_pagamento, situacao, valor, id_usuario, id_categoria) VALUES ('Internet', '2025-06-05', '2025-06-05', 'paga', 1200.00, 1, 1);
 INSERT INTO tb_despesa(descricao, data_vencimento, data_pagamento, situacao, valor, id_usuario, id_categoria) VALUES ('Conta de Água', '2025-06-10', '2025-06-09', 'paga', 85.75, 2, 2);
@@ -17,4 +18,4 @@ INSERT INTO tb_despesa(descricao, data_vencimento, data_pagamento, situacao, val
 
 INSERT INTO tb_receita(data_entrada, id_usuario, id_categoria, valor) VALUES ('2025-06-11', 3, 6, 1500.00);
 INSERT INTO tb_receita(data_entrada, id_usuario, id_categoria, valor) VALUES ('2025-06-09', 1, 7, 600.00);
-INSERT INTO tb_receita(data_entrada, id_usuario, id_categoria, valor) VALUES ('2025-06-09', 4, 6, 2000.00);
+INSERT INTO tb_receita(data_entrada, id_usuario, id_categoria, valor) VALUES ('2025-06-09', 4, 8, 2000.00);
